28f9a6bc296fea4e71d97e07cede1cd9186ff817
dwilliams56 <dwilliams56@murraystate.edu>
2022-05-09 05:25:51 -0500

    Final

---
+++ src/app/components/login-card/login-card.component.ts
@@ -0,0 +1,48 @@
+import { AuthenticationService } from '../../services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { PostLogsService } from './../../services/post-logs.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'login-card',
+  templateUrl: './login-card.component.html',
+  styleUrls: ['./login-card.component.css']
+})
+export class LoginCardComponent implements OnInit {
+
+  public user: string = '';
+  public password: string = '';
+  hide = true;
+
+  constructor(private service: AuthenticationService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    var User = this.user;
+    var password = this.password;
+
+    //testing purposes
+    console.log(User);
+    console.log(password);
+
+    var token = this.service.userLogin(User, password);
+    console.log(token);
+    this.gotoLogin();
+  }
+
+  gotoLogin() {
+    this.router.navigate(['/login']).then( (e) => {
+      if (e) {
+        console.log("Navigation is successful!");
+      } else {
+        console.log("Navigation has failed!");
+      }
+    });
+  }
+
+
+
+
+}
